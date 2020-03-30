12ad2e1e327f6ecc1509ef3bdd4918ee1b33500c
AndresLostaunau <39714785+AndresLostaunau@users.noreply.github.com>
2020-03-30 17:40:48 -0500

    selection sort

--- main.cpp
+++ main.cpp
@@ -15,14 +15,33 @@ int* insertSort(int array[]){
     return array;
 }
 
+int* selectionSort(int array[]){
+    int aux, j, min;
+    for(int i = 0; i < (n-1); i++){
+        min = i;
+        for(j = i+1; j < n; j++){
+            if(array[j] < array[min]){
+                min = j;
+            }
+        }
+        aux = array[min];
+        array[min] = array[i];
+        array[i] = aux;
+    }
+    return array;
+}
+
 void displayArray(int array[]){
     for(int i = 0; i < n; i++){
         cout<<array[i]<<",";
     }
+    cout<<endl;
 }
 
 int main(int, char*[]){
     int* insertArray = insertSort(sampleArray);
     displayArray(insertArray);
+    int* selectArray = selectionSort(sampleArray);
+    displayArray(selectArray);
     return 1;
 }
